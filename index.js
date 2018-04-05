dcd4ba6c72621419c27c5088ae415aedd058133a
Jorge Manzo <manzoj@oregonstate.edu>
2018-04-05 14:48:10 -0700

    Needed render function

--- index.js
+++ index.js
@@ -1,11 +1,9 @@
 
-import vue from 'Vue'
-
-vue.component('app', {
-	template: '<p>Hello World</p>'
-})
+import Vue from 'Vue'
+import App from './app.vue'
 
 
 let app1 = new Vue({
-	el: 'target-div'
+	el: '#target-div',
+	render: h => h(App)
 })
